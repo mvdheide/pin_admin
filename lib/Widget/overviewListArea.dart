d67e30cf7f62ffb49b3974f5b5a019a47e5724af
Your name <you@example.com>
2021-04-17 15:56:10 +0200

    separated DatabaseProvider, separated detailArea&detailProvider, removed old out-commented code,

--- lib/Widget/overviewListArea.dart
+++ lib/Widget/overviewListArea.dart
@@ -7,41 +7,27 @@ import 'package:pin_admin/models/textFieldModel.dart';
 import 'package:provider/provider.dart';
 
 class OverViewListArea extends StatelessWidget {
-  //   final List<String> textFieldValues;
-  // final int lastButtonClicked;
-  // final bool asc;
-  // OverViewModel(
-  //     {@required this.textFieldValues, this.lastButtonClicked, this.asc})
-  //     : super();
   final int deviceModel;
-  // final OverViewModel overViewModel;
-  OverViewListArea.phone(
-      // this.overViewModel,
-      )
-      : deviceModel = OverViewModel.PHONE_MODE;
-  OverViewListArea.tablet(
-      // this.overViewModel,
-      )
-      : deviceModel = OverViewModel.TABLET_MODE; //,
-  // overViewModel = null;
+
+//----------------------------------------------------------------------------//
+
+  OverViewListArea.phone() : deviceModel = OverViewModel.PHONE_MODE;
+
+//----------------------------------------------------------------------------//
+
+  OverViewListArea.tablet() : deviceModel = OverViewModel.TABLET_MODE;
+
+//----------------------------------------------------------------------------//
 
   @override
   Widget build(BuildContext context) {
     print("overviewArea - Build start");
     return Expanded(
-      // child: Padding(
-      // padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-      // color: Theme.of(context).primaryColor,
       child: Container(
         decoration: BoxDecoration(
             color: Theme.of(context).cardColor,
             borderRadius: BorderRadius.all(Radius.circular(8))),
-        // color: Theme.of(context).cardColor,
-        child:
-            // Consumer<OverViewModel>(
-            //   builder: (context, model, chld) {
-            //     return
-            FutureBuilder<List<List<String>>>(
+        child: FutureBuilder<List<List<String>>>(
           future: Provider.of<OverViewModel>(context, listen: false)
               .updateOverviewList(
             Provider.of<TextFieldModel>(context).getAllTextFieldValues(),
@@ -51,7 +37,7 @@ class OverViewListArea extends StatelessWidget {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               print("overviewArea: future start");
-              int overviewItemCount = snapshot.data.length;
+              int overviewItemCount = snapshot.data!.length;
 
               return Column(
                 children: [
@@ -68,40 +54,16 @@ class OverViewListArea extends StatelessWidget {
                             model.showDetails(int.parse(item[5]));
                             if (deviceModel == OverViewModel.PHONE_MODE) {
                               Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          PhoneDetailPage(model
-                                              // int.parse(item[5])
-                                              )));
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PhoneDetailPage(model),
+                                ),
+                              );
                             }
-                            // else if (deviceModel ==
-                            //     OverViewModel.TABLET_MODE) {
-                            //   model.showDetails(int.parse(item[5]));
-                            // }
-
-                            // context.
-                            // setState(() {
-                            // _detailsAreakey.currentState
-                            //     .updateDetailsArea(item['_id']);
-                            // _selectedID = item['_id'];
-                            // });
-
-                            // Navigator.push(
-                            //     context,
-                            //     MaterialPageRoute(
-                            //         builder: (context) => Details(item['_id'])));
                           },
                           child: Container(
-                            // padding: const EdgeInsets.only(
-                            //     left: 8.0, right: 8.0, bottom: 4.0),
-
-                            // padding: const EdgeInsets.only(bottom: 4.0),
                             color: Theme.of(context).cardColor,
-                            child:
-                                // ListTile(
-                                //   title:
-                                Column(
+                            child: Column(
                               children: <Widget>[
                                 Row(
                                   children: <Widget>[
@@ -135,7 +97,6 @@ class OverViewListArea extends StatelessWidget {
                                 ),
                               ],
                             ),
-                            // ),
                           ),
                         );
                       },
@@ -153,16 +114,10 @@ class OverViewListArea extends StatelessWidget {
             }
           },
         ),
-        // },
       ),
-      // ),
     );
-    // );
-    //           }
-    //         },
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
+
+//----------------------------------------------------------------------------//
+
 }
